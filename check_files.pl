f6ed23ed4c2b155df4fd693fc8fcc9fbbaee2ec4
Dan Zeman <zeman@ufal.mff.cuni.cz>
2015-11-03 10:27:32 +0100

    Reading metadata from README.

--- check_files.pl
+++ check_files.pl
@@ -8,6 +8,15 @@ use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
+use Getopt::Long;
+
+# Include reports on future repositories (not scheduled for the upcoming release)?
+# (If there is no README file, we will include the repository in the report and complain about the missing README.)
+my $include_future = 0;
+GetOptions
+(
+    'future' => \$include_future
+);
 
 # This script expects to be invoked in the folder in which all the UD_folders
 # are placed.
@@ -76,6 +85,24 @@ foreach my $folder (@folders)
         {
             $langcode = $langcodes{$language};
             chdir($folder) or die("Cannot enter folder $folder");
+            # Read the README file first. We need to know whether this repository is scheduled for the upcoming release.
+            if(!-f 'README.txt' && !-f 'README.md')
+            {
+                print("$folder: missing README.txt|md\n");
+                $n_errors++;
+            }
+            if(-f 'README.txt' && -f 'README.md')
+            {
+                print("$folder: both README.txt and README.md are present\n");
+                $n_errors++;
+            }
+            my $metadata = read_readme($folder);
+            if(!$metadata->{release} && !$include_future)
+            {
+                chdir('..') or die("Cannot return to the upper folder");
+                next;
+            }
+            # Look for the other files in the repository.
             opendir(DIR, '.') or die("Cannot read the contents of the folder $folder");
             my @conllufiles = grep {-f $_ && m/\.conllu$/} (readdir(DIR));
             my $n = scalar(@conllufiles);
@@ -100,14 +127,9 @@ foreach my $folder (@folders)
                     print("$folder: expected $expected_n CoNLL-U files, found $n\n");
                     $n_errors++;
                 }
-                if(!-f 'README.txt' && !-f 'README.md')
-                {
-                    print("$folder: missing README.txt|md\n");
-                    $n_errors++;
-                }
                 if(!-f 'LICENSE.txt')
                 {
-                    print("$folder: missing LICENSE.txt\n");
+                    print("$folder: missing LICENSE.txt (README says license is '$metadata->{License}')\n");
                     $n_errors++;
                 }
                 # Check the names of the data files.
@@ -153,3 +175,88 @@ print("$n_folders_with_data are git repositories and contain data.\n");
 my @languages = sort(keys(%languages_with_data));
 print(scalar(@languages), " languages with data: ", join(' ', @languages), "\n");
 print("$n_errors errors must be fixed.\n") if($n_errors>0);
+
+
+
+#------------------------------------------------------------------------------
+# Reads the README file of a treebank and finds the metadata lines. Example:
+#=== Machine-readable metadata ================================================
+#Documentation status: partial
+#Data source: automatic
+#Data available since: UD v1.2
+#License: CC BY-NC-SA 2.5
+#Genre: fiction
+#Contributors: Celano, Giuseppe G. A.; Zeman, Daniel
+#==============================================================================
+#------------------------------------------------------------------------------
+sub read_readme
+{
+    # Assumption: The current folder is a UD data repository.
+    # Nevertheless, we want to know the folder name so we can use it in messages.
+    my $folder = shift;
+    my $filename = (-f 'README.txt') ? 'README.txt' : 'README.md';
+    open(README, $filename) or return;
+    my %metadata;
+    my @attributes = ('Documentation status', 'Data source', 'Data available since', 'License', 'Genre', 'Contributors');
+    my $attributes_re = join('|', @attributes);
+    while(<README>)
+    {
+        s/\r?\n$//;
+        s/^\s+//;
+        s/\s+$//;
+        s/\s+/ /g;
+        if(m/^($attributes_re):\s*(.*)$/i)
+        {
+            my $attribute = $1;
+            my $value = $2;
+            $value = '' if(!defined($value));
+            if(exists($metadata{$attribute}))
+            {
+                print("WARNING: Repeated definition of '$attribute' in $folder/$filename\n");
+            }
+            $metadata{$attribute} = $value;
+            if($attribute eq 'Data available since')
+            {
+                if($metadata{$attribute} =~ m/^UD v1\.(\d)$/ && $1 <= 2)
+                {
+                    $metadata{'release'} = 1;
+                }
+            }
+        }
+    }
+    close(README);
+    if(!$metadata{'release'} && !$include_future)
+    {
+        return;
+    }
+    # Check the values of the metadata.
+    foreach my $attribute (@attributes)
+    {
+        if(!exists($metadata{$attribute}))
+        {
+            print("WARNING: Attribute '$attribute' not defined in $folder/$filename\n");
+        }
+        elsif($attribute eq 'License')
+        {
+            if($metadata{$attribute} eq '')
+            {
+                print("WARNING: unknown license in $folder/$filename\n");
+            }
+        }
+        elsif($attribute eq 'Genre')
+        {
+            if($metadata{$attribute} eq '')
+            {
+                print("WARNING: unknown genre in $folder/$filename\n");
+            }
+        }
+        elsif($attribute eq 'Contributors')
+        {
+            if($metadata{$attribute} eq '')
+            {
+                print("WARNING: unknown contributors in $folder/$filename\n");
+            }
+        }
+    }
+    return \%metadata;
+}
