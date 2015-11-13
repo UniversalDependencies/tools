3c9b6deb4a133a42c4fcabecabcf2bdc04bcb00b
Dan Zeman <zeman@ufal.mff.cuni.cz>
2015-11-13 18:17:28 +0100

    Do not report "not-to-release" as an extra file.

--- check_files.pl
+++ check_files.pl
@@ -190,7 +190,7 @@ foreach my $folder (@folders)
                 }
                 grep
                 {
-                    !m/^(\.\.?|\.git(ignore)?|README\.(txt|md)|LICENSE\.txt|$prefix-(train|dev|test)\.conllu|cs-ud-train-[clmv]\.conllu|stats\.xml)$/
+                    !m/^(\.\.?|\.git(ignore)?|not-to-release|README\.(txt|md)|LICENSE\.txt|$prefix-(train|dev|test)\.conllu|cs-ud-train-[clmv]\.conllu|stats\.xml)$/
                 }
                 (@files);
                 if(scalar(@extrafiles)>0)
