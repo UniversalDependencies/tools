c7d6f7dc8e102b25564221e592f7f87816e74028
Dan Zeman <zeman@ufal.mff.cuni.cz>
2015-11-18 13:38:46 +0100

    Joakim says we should also have non-empty POSTAG.

--- conllu_to_conllx.pl
+++ conllu_to_conllx.pl
@@ -16,12 +16,17 @@ while(<>)
     next if(m/^\#/);
     # Discard lines of fused surface tokens. Syntactic words will be the node-level unit in the output file.
     next if(m/^\d+-\d+/);
-    s/\r?\n$//;
-    # The last two columns ([8] and [9]) had different meaning in CoNLL-X.
-    # In many cases it is probably harmless to keep their contents from CoNLL-U, but some tools may rely on their expectations about these columns,
-    # especially in [8] they may require either '_' or a numeric value. Let's erase the contents of these columns to be on the safe side.
-    my @fields = split(/\t/, $_);
-    $fields[8] = $fields[9] = '_';
-    $_ = join("\t", @fields)."\n";
+    if(m/\t/)
+    {
+        s/\r?\n$//;
+        my @fields = split(/\t/, $_);
+        # Some tools (MaltParser?) may not expect POSTAG to be empty if we have non-empty CPOSTAG. Copy CPOSTAG to POSTAG if POSTAG is empty.
+        $fields[4] = $fields[3] if($fields[4] eq '_');
+        # The last two columns ([8] and [9]) had different meaning in CoNLL-X.
+        # In many cases it is probably harmless to keep their contents from CoNLL-U, but some tools may rely on their expectations about these columns,
+        # especially in [8] they may require either '_' or a numeric value. Let's erase the contents of these columns to be on the safe side.
+        $fields[8] = $fields[9] = '_';
+        $_ = join("\t", @fields)."\n";
+    }
     print;
 }
