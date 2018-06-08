24c6f868d07f8933dfe7d032c937af14f59de936
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2018-06-08 20:10:54 -0300

    Improving check_sentence_ids.pl
    - Sentences without a sent_id after a correct sentence (a sentence
      with a sent_id) are now reported as lacking a sent_id, not as having
      a duplicate sent_id
      (see test-cases/nonvalid/no-sent_id.conllu)
    
    - Extra empty lines are now reported explicitly, instead of reporting
      as duplicate sent_ids
    
    - Sentences with multiple sent_id values are now reported
      explicitly (see test-cases/nonvalid/multiple-sent_id.conllu)

--- check_sentence_ids.pl
+++ check_sentence_ids.pl
@@ -19,30 +19,49 @@ binmode(STDERR, ':utf8');
 
 my %hash;
 my $current_id = '';
-my $current_id_lineno = 0;
 my $lineno = 0;
+my $in_sentence = 0;
+my $first_sentence_line = 0;
 while(<>)
 {
     $lineno++;
     s/\r?\n$//;
-    if(m/^\#\s*sent_id\s*=\s*(.+)/)
+    if($in_sentence == 0)
     {
-        $current_id = $1;
-        $current_id_lineno = $lineno;
-    }
-    elsif(m/^\s*$/)
-    {
-        if($current_id eq '')
+        if(m/^$/)
         {
-            print STDERR ("Missing sent_id at line no. $lineno.\n");
-        }
-        elsif(exists($hash{$current_id}))
-        {
-            print STDERR ("Duplicate sent_id '$current_id' at line no. $current_id_lineno (first occurrence at line no. $hash{$current_id}).\n");
+            print STDERR ("Unwarranted empty line at line no. $lineno.\n");
         }
         else
         {
-            $hash{$current_id} = $current_id_lineno;
+            $in_sentence = 1;
+            $first_sentence_line = $lineno ;
+        }
+    }
+    if($in_sentence == 1 and m/^$/)
+    {
+        if($current_id eq '')
+        {
+            print STDERR ("Missing sent_id at sentence beginning at line no. $first_sentence_line.\n");
+        }
+        $in_sentence = 0;
+        $current_id = '';
+        $first_sentence_line = -1;
+    }
+    if(m/^\#\s*sent_id\s*=\s*(.+)/)
+    {
+        if(not($current_id eq ''))
+        {
+            print STDERR ("Multiple 'sent_id' values at sentence beginning at line no. $first_sentence_line.\n");
+        }
+        $current_id = $1;
+        if(exists($hash{$current_id}))
+        {
+            print STDERR ("Duplicate sent_id '$current_id' at line no. $lineno (first occurrence at line no. $hash{$current_id}).\n");
+        }
+        else
+        {
+            $hash{$current_id} = $lineno;
         }
     }
 }
