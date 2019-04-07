072dec08e5fed7acf01a44c72c2c0af5feed4e49
Christopher Manning <manning@cs.stanford.edu>
2019-04-07 10:45:11 -0700

    Add passive get auxiliary

--- validate.py
+++ validate.py
@@ -1222,7 +1222,8 @@ def validate_auxiliary_verbs(cols, children, nodes, line, lang):
     if cols[UPOS] == 'AUX' and cols[LEMMA] != '_':
         ###!!! In the future, lists like this one will be read from a file.
         auxdict = {
-            'en':  ['be', 'have', 'do', 'will', 'would', 'may', 'might', 'can', 'could', 'shall', 'should', 'must'],
+            # ChrisManning 2019/04: Allow 'get' as aux for get passive construction
+            'en':  ['be', 'have', 'do', 'will', 'would', 'may', 'might', 'can', 'could', 'shall', 'should', 'must', 'get'],
             'nl':  ['zijn', 'hebben', 'worden', 'kunnen', 'mogen', 'zullen', 'moeten'],
             'de':  ['sein', 'haben', 'werden', 'dürfen', 'können', 'mögen', 'wollen', 'sollen', 'müssen'],
             'sv':  ['vara', 'ha', 'bli', 'komma', 'få', 'kunna', 'kunde', 'vilja', 'torde', 'behöva', 'böra', 'skola', 'måste'],
