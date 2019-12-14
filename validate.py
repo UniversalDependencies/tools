3b076939923f4bdded28480a5ac094dafc916a38
Dan Zeman <zeman@ufal.mff.cuni.cz>
2019-12-14 22:56:56 +0100

    Arabic auxiliaries.

--- validate.py
+++ validate.py
@@ -1723,7 +1723,10 @@ def validate_auxiliary_verbs(cols, children, nodes, line, lang):
             'wbp': ['ka'],
             # Afro-Asiatic languages.
             'mt':  ['kien', 'għad', 'għadx', 'ġa', 'se', 'ħa', 'qed'],
-            'ar':  ['كَان', 'لَيس', 'لسنا', 'هُوَ', 'سَ', 'قَد'],
+            # رُبَّمَا rubbamā "maybe, perhaps" is a modal auxiliary
+            # مَا mā "not" is negation. Maybe it should be PART/advmod rather than AUX/aux?
+            # هَل hal "whether" is a question particle. Maybe it should be PART/advmod rather than AUX/aux?
+            'ar':  ['كَان', 'لَيس', 'لسنا', 'هُوَ', 'سَ', 'قَد', 'مَا', 'هَل', 'رُبَّمَا'],
             'he':  ['היה', 'הוא', 'זה'],
             'aii': ['ܗܵܘܹܐ', 'ܟܸܐ', 'ܟܹܐ', 'ܟܲܕ', 'ܒܸܬ', 'ܒܹܬ', 'ܒܸܕ', 'ܒ', 'ܦܵܝܫ', 'ܡܵܨܸܢ', 'ܩܲܡ'],
             # https://universaldependencies.org/cop/auxiliaries.html (as per mail from Amir 19.11.2019)
