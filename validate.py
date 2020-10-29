6278ca7bdaa3a35ee83cbe5392ab1b0266bb09f7
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-10-29 21:02:53 +0100

    Added Icelandic auxiliaries "blífa" and "kunna".

--- validate.py
+++ validate.py
@@ -1625,10 +1625,10 @@ def validate_auxiliary_verbs(cols, children, nodes, line, lang):
             'nl':  ['zijn', 'hebben', 'worden', 'krijgen', 'kunnen', 'mogen', 'zullen', 'moeten'],
             'de':  ['sein', 'haben', 'werden', 'dürfen', 'können', 'mögen', 'wollen', 'sollen', 'müssen'],
             'sv':  ['vara', 'ha', 'bli', 'komma', 'få', 'kunna', 'kunde', 'vilja', 'torde', 'behöva', 'böra', 'skola', 'måste', 'må', 'lär', 'do'], # Note: 'do' is English and is included because of code switching (titles of songs).
-            'no':  ['være', 'vere', 'ha', 'verte', 'bli', 'få', 'kunne', 'ville', 'vilje', 'tørre', 'tore', 'burde', 'skulle', 'måtte'],
+            'no':  ['være', 'vere', 'ha', 'bli', 'verte', 'få', 'kunne', 'ville', 'vilje', 'tørre', 'tore', 'burde', 'skulle', 'måtte'],
             'da':  ['være', 'have', 'blive', 'kunne', 'ville', 'turde', 'burde', 'skulle', 'måtte'],
             'fo':  ['vera', 'hava', 'verða', 'koma', 'fara', 'kunna', 'skula', 'vara', 'kunnu', 'mega'],
-            'is':  ['vera', 'hafa', 'verða', 'geta', 'fá', 'mega', 'vilja', 'munu', 'skulu', 'eiga'],
+            'is':  ['vera', 'hafa', 'blífa', 'verða', 'geta', 'fá', 'kunna', 'mega', 'vilja', 'munu', 'skulu', 'eiga'],
             'got': ['wisan'],
             # DZ: The Portuguese list is much longer than for the other Romance languages
             # and I suspect that maybe not all these verbs are auxiliary in the UD sense,
