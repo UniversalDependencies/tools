c1a6f91d3d4787d4160d9988877a449339309c10
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-05-05 11:55:46 +0200

    Updated default parameters.

--- check_files.pl
+++ check_files.pl
@@ -39,15 +39,15 @@ my $recompute_stats = 0;
 # Tag all repositories with the new release? (The $tag variable is either empty or it contains the tag.)
 my $tag = ''; # example: 'r1.0'
 # Number of the current release as it is found in README files. Repositories targeting a later release will not be included.
-my $current_release = 2.7;
+my $current_release = 2.8;
 # Month and year when the next release is expected. We use it in the announcement.
-my $next_release_expected = 'May 2021';
+my $next_release_expected = 'November 2021';
 my $announcement_min_size = 'less than 1,000 tokens';
 my $announcement_max_size = 'over 3 million tokens';
 # Path to the previous release is needed to compare the number of sentences and words.
 # zen:/net/data/universal-dependencies-1.2
 # mekong:C:\Users\Dan\Documents\Lingvistika\Projekty\universal-dependencies\release-1.2
-my $oldpath = '/net/data/universal-dependencies-2.6';
+my $oldpath = '/net/data/universal-dependencies-2.7';
 ###!!! Also check the new_treebanks_by_release hash in check_metadata()!
 GetOptions
 (
