9ae5b3f7b6647dadbfcdfb4978a31d3311b62e15
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-05-13 23:37:52 +0200

    HTML instead of MarkDown.

--- survey_features.pl
+++ survey_features.pl
@@ -238,27 +238,12 @@ sub print_markdown
 {
     my $hash = shift;
     my @features = sort(keys(%{$hash}));
-    # The CSS style:
-    # Normally we would want position (of the bullet) outside, which is the default.
-    # But if we want to manipulate the indentation of the first line, we want the
-    # bullet to be a part of it so that it moves along, hence position: inside.
-    # The negative text-indent moves the start of the first line of each item to
-    # the left. The margin-left flips the logic: the first line will start at
-    # position 0, while the start of all other lines is moved to the right.
     print <<EOF
 ---
 layout: base
 title:  'Features and Values'
 udver: '2'
 ---
-
-<style>
-  ul {
-    list-style-position: inside;
-    text-indent: -3em;
-    margin-left: 3em;
-  }
-</style>
 
 This is an automatically generated list of features and values (both universal and language-specific) that occur in the UD data.
 EOF
@@ -268,13 +253,21 @@ EOF
         my @values = sort(keys(%{$hash->{$f}}));
         print("\#\# $f\n\n");
         print("[$f]()\n\n");
+        # The CSS style:
+        # Normally we would want position (of the bullet) outside, which is the default.
+        # But if we want to manipulate the indentation of the first line, we want the
+        # bullet to be a part of it so that it moves along, hence position: inside.
+        # The negative text-indent moves the start of the first line of each item to
+        # the left. The margin-left flips the logic: the first line will start at
+        # position 0, while the start of all other lines is moved to the right.
+        print("<ul style='list-style-position: inside; text-indent: -3em; margin-left: 3em;'>\n");
         foreach my $v (@values)
         {
             my @folders = sort(keys(%{$hash->{$f}{$v}}));
             my @folders_with_frequencies = map {"$_&nbsp;($hash->{$f}{$v}{$_})"} (@folders);
-            print('* `'.$v.'` ('.join(', ', @folders_with_frequencies).')'."\n");
+            print('<li> `'.$v.'` ('.join(', ', @folders_with_frequencies).')</li>'."\n");
         }
-        print("\n");
+        print("</ul>\n\n");
     }
 }
 
