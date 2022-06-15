5bb4e6d896404d66fd22af86f7b84ecb80eb4d79
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-06-15 16:00:40 +0200

    A script to cut a section from a CoNLL-U file.

---
+++ conllu_cut.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+# Takes a CoNLL-U file, cuts a section out of it, and prints the section.
+# The section is identified by the id of the first and the last sentence.
+# Copyright © 2022 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# License: GNU GPL
+
+use utf8;
+use open ':utf8';
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+binmode(STDERR, ':utf8');
+use Getopt::Long;
+
+sub usage
+{
+    print STDERR ("Usage: perl conllu_cut.pl --first sent350 --last sent367 < whole.conllu > section.conllu\n");
+    print STDERR ("       where sent350 and sent367 are sent_ids of the first and the last sentence included in the output\n");
+    print STDERR ("       if --first is omitted, section starts at the beginning of the input file\n");
+    print STDERR ("       if --last is omitted, section ends at the end of the input file\n");
+}
+
+my $firstsid;
+my $lastsid;
+GetOptions
+(
+    'first=s' => \$firstsid,
+    'last=s'  => \$lastsid
+);
+
+my $inside = !defined($firstsid);
+my @sentence = ();
+while(<>)
+{
+    s/\r?\n$//;
+    push(@sentence, $_);
+    if(m/^\s*$/)
+    {
+        process_sentence(@sentence);
+        @sentence = ();
+    }
+}
+# If we encountered the end of the section, we exited the script in process_sentence().
+# Being here means that we either want the section to reach the end of the input,
+# or we failed to find the beginning of the section.
+if(!$inside)
+{
+    print STDERR ("WARNING: The first sentence of the section, '$firstsid', was not found.\n");
+}
+
+sub process_sentence()
+{
+    my @sentence = @_;
+    # Get the sentence id.
+    my $sid;
+    foreach my $line (@sentence)
+    {
+        if($line =~ m/^\#\s*sent_id\s*=\s*(\S+)$/)
+        {
+            $sid = $1;
+            last;
+        }
+    }
+    if(!defined($sid))
+    {
+        print STDERR ("WARNING: Sentence has no sent_id\n");
+        $sid = '';
+    }
+    if(!$inside && $sid eq $firstsid)
+    {
+        $inside = 1;
+    }
+    if($inside)
+    {
+        print(join("\n", @sentence), "\n");
+        if(defined($lastsid) && $sid eq $lastsid)
+        {
+            # No need to read the rest of the input.
+            exit(0);
+        }
+    }
+}
