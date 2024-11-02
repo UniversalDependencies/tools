23d29cfb374727b375fcbf76f8e303f06ab948c9
Dan Zeman <zeman@ufal.mff.cuni.cz>
2024-11-02 21:59:08 +0100

    Recursive.

--- scan_local_repos.pl
+++ scan_local_repos.pl
@@ -12,12 +12,92 @@ binmode(STDERR, ':utf8');
 use Cwd;
 
 my $path = getcwd();
-opendir(DIR, $path) or die("Cannot read folder '$path': $!");
-my @subfolders = grep {-d $_ && !m/^\.\.?$/} (readdir(DIR));
-closedir(DIR);
-foreach my $sf (@subfolders)
+scan_path($path);
+
+
+
+#------------------------------------------------------------------------------
+# Scans subfolders of given path.
+#------------------------------------------------------------------------------
+sub scan_path
 {
-    chdir("$path/$sf") or die("Cannot change to folder '$sf': $!");
+    my $path = shift;
+    opendir(DIR, $path) or die("Cannot read folder '$path': $!");
+    my @subfolders = grep {-d $_ && !m/^\.\.?$/} (readdir(DIR));
+    closedir(DIR);
+    foreach my $sf (@subfolders)
+    {
+        print STDERR ("$path/$sf\n");
+        chdir("$path/$sf") or die("Cannot change to folder '$sf': $!");
+        my $git = is_git_repo();
+        if($git)
+        {
+            my $origin = get_git_url();
+            if(defined($origin))
+            {
+                print("$path/$sf = $origin\n");
+            }
+            else
+            {
+                print("$path/$sf = CANNOT FIND REMOTE ORIGIN\n");
+            }
+        }
+        else
+        {
+            my $svn = is_svn_repo();
+            if($svn)
+            {
+                my $origin = get_svn_url();
+                if(defined($origin))
+                {
+                    print("$path/$sf = $origin\n");
+                }
+                else
+                {
+                    print("$path/$sf = CANNOT FIND SVN URL\n");
+                }
+            }
+            else
+            {
+                print("$path/$sf = NOT A GIT/SVN REPO\n");
+                # Scan recursively.
+                scan_path("$path/$sf");
+            }
+        }
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Finds out whether the current folder is a git repository.
+#------------------------------------------------------------------------------
+sub is_git_repo
+{
+    ###!!! We should examine other possibilities, e.g. that the git command cannot be executed.
+    my $status = `git status 2>&1`;
+    return $status !~ m/fatal: not a git repository/s;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Finds out whether the current folder is an svn repository.
+#------------------------------------------------------------------------------
+sub is_svn_repo
+{
+    ###!!! We should examine other possibilities, e.g. that the svn command cannot be executed.
+    my $status = `svn status 2>&1`;
+    return $status !~ m/svn: warning:.*is not a working copy/s;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Assuming the current folder is a git repo, figures out its origin URL.
+#------------------------------------------------------------------------------
+sub get_git_url
+{
     my $origin;
     open(GIT, "git remote -v |") or die("Cannot read from git: $!");
     while(<GIT>)
@@ -25,20 +105,30 @@ foreach my $sf (@subfolders)
         if(m/^origin\s+(\S+)/)
         {
             $origin = $1;
+            last;
         }
     }
     close(GIT);
-    if(defined($origin))
+    return $origin;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Assuming the current folder is an svn repo, figures out its origin URL.
+#------------------------------------------------------------------------------
+sub get_svn_url
+{
+    my $origin;
+    open(SVN, "svn info |") or die("Cannot read from git: $!");
+    while(<SVN>)
     {
-        print("$origin\n");
-        chdir("$path/00") or die("Cannot change to folder '00': $!");
-        system("git clone $origin");
-        chdir("$path/00/$sf");
-        # Changing to dev will not work for docs, docs-auto, tools, and LICENSE.
-        system("git checkout dev");
+        if(m/^URL:\s+(\S+)/)
+        {
+            $origin = $1;
+            last;
+        }
     }
-    else
-    {
-        print("CANNOT FIND REMOTE ORIGIN FOR '$sf'\n");
-    }
+    close(SVN);
+    return $origin;
 }
