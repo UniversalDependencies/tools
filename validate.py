104f50afe87b1b13573a9ffaf86d1c4a7df48edd
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-06-18 20:30:45 +0200

    Revert. We need to set up the environment on quest first.

--- validate.py
+++ validate.py
@@ -1765,16 +1765,17 @@ def validate_misc(tree):
 
 ###!!! Just testing now: Can we switch to Udapi when building and searching the tree?
 
-import udapi.block.read.conllu
-#import read_tree_from_lines
-
-def build_tree_udapi(lines):
-    conllu_reader = udapi.block.read.conllu.Conllu()
-    root = conllu_reader.read_tree_from_lines(lines)
-    descendants = root.descendants
-    text = root.compute_text()
-    print(f"The tree has {len(descendants)} nodes: {text}")
-
+# =============================================================================
+# import udapi.block.read.conllu
+# 
+# def build_tree_udapi(lines):
+#     conllu_reader = udapi.block.read.conllu.Conllu()
+#     root = conllu_reader.read_tree_from_lines(lines)
+#     descendants = root.descendants
+#     text = root.compute_text()
+#     print(f"The tree has {len(descendants)} nodes: {text}")
+# 
+# =============================================================================
 
 
 def build_tree(sentence):
