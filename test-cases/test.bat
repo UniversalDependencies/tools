eb1860d74d15402252224f401af39d04214ed6d2
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-06-24 07:23:37 +0200

    The old tests that we have so far are only for levels 1 and 2.

--- test-cases/test.bat
+++ test-cases/test.bat
@@ -1,8 +1,11 @@
 @echo off
+REM Test the UD validator on a set of short CoNLL-U files.
+REM The log file is versioned, so we can check git diff and see if it is OK.
+REM Usage: tools\test-cases\test.bat > tools\test-cases\test.log 2>&1
 SET "SCRIPT_DIR=%~dp0"
 for %%f in ("%SCRIPT_DIR%nonvalid\*.conllu") do (
     @echo --------------------------------------------------------------------------------
     @echo Processing file: %%f
-    python %SCRIPT_DIR%..\validate.py --lang en "%%f"
+    python %SCRIPT_DIR%..\validate.py --level 2 --lang ud "%%f"
     @echo.
 )
