c77e6b72f70549fe0945109fa0531b3be2a54117
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-09-12 09:52:39 +0200

    Repurposing the script to PUD.

--- add_parallel_to_all_treebanks.pl
+++ add_parallel_to_all_treebanks.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# Adds Parallel: no to the README of all UD treebanks, unless they already have the Parallel: line.
+# Adds Parallel: pud to the README of all PUD treebanks.
 # Copyright © 2025 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # License: GNU GPL
 
@@ -13,39 +13,28 @@ use udlib;
 my @folders = udlib::list_ud_folders(); # in the current folder
 foreach my $folder (@folders)
 {
+    next unless($folder =~ m/-PUD$/);
     print("$folder\n");
     # Get the most recent revision of the folder.
     chdir($folder) or die("Cannot change to folder '$folder': $!");
     system('git pull --no-edit');
     chdir('..');
-    my $metadata = udlib::read_readme($folder);
-    if(!exists($metadata->{Parallel}))
+    #my $metadata = udlib::read_readme($folder);
+    my $filename = (-f "$folder/README.txt") ? "$folder/README.txt" : "$folder/README.md";
+    my $contents;
+    open(my $in, $filename) or die("Cannot read '$filename': $!");
+    while(<$in>)
     {
-        # No Parallel metadata found. Insert the default, Parallel: no.
-        my $filename = (-f "$folder/README.txt") ? "$folder/README.txt" : "$folder/README.md";
-        print(" ----------------------> going to add Parallel to $filename\n");
-        my $contents;
-        open(my $in, $filename) or die("Cannot read '$filename': $!");
-        while(<$in>)
-        {
-            $contents .= $_;
-            if(m/^Includes text:/)
-            {
-                $contents .= "Parallel: no\n";
-            }
-        }
-        close($in);
-        open(my $out, ">$filename") or die("Cannot write '$filename': $!");
-        print $out $contents;
-        close($out);
-        # Push the changes to GitHub.
-        chdir($folder) or die("Cannot change to folder '$folder': $!");
-        system("git commit -a -m 'Added Parallel to README.' ; git push");
-        chdir('..');
+        s/^Parallel:\s*no$/Parallel: pud/;
+        $contents .= $_;
     }
-    else
-    {
-        print("README already contains Parallel: $metadata->{Parallel}\n");
-    }
+    close($in);
+    open(my $out, ">$filename") or die("Cannot write '$filename': $!");
+    print $out $contents;
+    close($out);
+    # Push the changes to GitHub.
+    chdir($folder) or die("Cannot change to folder '$folder': $!");
+    system("git commit -a -m 'Added Parallel to README.' ; git push");
+    chdir('..');
     print("\n");
 }
