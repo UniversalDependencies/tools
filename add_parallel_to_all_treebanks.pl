cffcbd7adb363325b1192012c22281099ac4ec22
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-09-12 10:06:03 +0200

    And now the data...

--- add_parallel_to_all_treebanks.pl
+++ add_parallel_to_all_treebanks.pl
@@ -18,22 +18,41 @@ foreach my $folder (@folders)
     # Get the most recent revision of the folder.
     chdir($folder) or die("Cannot change to folder '$folder': $!");
     system('git pull --no-edit');
-    chdir('..');
+    opendir(DIR, '.');
+    my @files = grep {m/\.conllu/} (readdir(DIR));
+    closedir(DIR);
     #my $metadata = udlib::read_readme($folder);
-    my $filename = (-f "$folder/README.txt") ? "$folder/README.txt" : "$folder/README.md";
-    my $contents;
-    open(my $in, $filename) or die("Cannot read '$filename': $!");
-    while(<$in>)
-    {
-        s/^Parallel:\s*no$/Parallel: pud/;
-        $contents .= $_;
-    }
-    close($in);
-    open(my $out, ">$filename") or die("Cannot write '$filename': $!");
-    print $out $contents;
-    close($out);
+    #my $filename = (-f "$folder/README.txt") ? "$folder/README.txt" : "$folder/README.md";
+    #my $contents;
+    #open(my $in, $filename) or die("Cannot read '$filename': $!");
+    #while(<$in>)
+    #{
+    #    s/^Parallel:\s*no$/Parallel: pud/;
+    #    $contents .= $_;
+    #}
+    #close($in);
+    #open(my $out, ">$filename") or die("Cannot write '$filename': $!");
+    #print $out $contents;
+    #close($out);
     # Push the changes to GitHub.
-    chdir($folder) or die("Cannot change to folder '$folder': $!");
+    foreach my $f (@files)
+    {
+        my $contents;
+        open(my $in, $f) or die("Cannot read '$f': $!");
+        while(<$in>)
+        {
+            $contents .= $_;
+            if(m/^\#\s*sent_id\s*=\s*([nw][0-9]+)$/)
+            {
+                $contents .= "\# parallel_id = pud/$1\n";
+            }
+        }
+        close($in);
+        open(my $out, ">$f") or die("Cannot write '$f': $!");
+        print $out $contents;
+        close($out);
+        die;
+    }
     system("git commit -a -m 'Added Parallel to README.' ; git push");
     chdir('..');
     print("\n");
