52464479496a4d0cfac23e1578ce03c58b823d7e
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-09-20 21:12:13 +0200

    New test log in the new location.

--- validator/tests/test-cases/test.bat
+++ validator/tests/test-cases/test.bat
@@ -1,13 +1,13 @@
 @echo off
 REM Test the UD validator on a set of short CoNLL-U files.
 REM The log file is versioned, so we can check git diff and see if it is OK.
-REM Usage: tools\test-cases\test.bat 2> tools\test-cases\test.log
+REM Usage: tools\validator\tests\test-cases\test.bat 2> tools\validator\tests\test-cases\test.log
 SET "SCRIPT_DIR=%~dp0"
 for %%f in ("%SCRIPT_DIR%invalid-level1-2\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 2 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 2 --lang ud "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
@@ -17,7 +17,7 @@ for %%f in ("%SCRIPT_DIR%invalid-level3\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 3 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 3 --lang ud "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
@@ -27,7 +27,7 @@ for %%f in ("%SCRIPT_DIR%invalid-level4-5\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 5 --lang cs "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 5 --lang cs "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
@@ -37,6 +37,6 @@ for %%f in ("%SCRIPT_DIR%valid\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 3 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 3 --lang ud "%%f"
     @echo. 1>&2
 )
