181c393e7762844289a36a975153be1df1c18e50
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-12-09 16:19:02 +0100

    Copying packaging files from another repository.

---
+++ udtools/src/udtools/__init__.py
@@ -0,0 +1,11 @@
+"""
+udtools.
+
+Python tools for Universal Dependencies.
+"""
+
+__version__ = "0.1.22"
+__author__ = 'Daniel Zeman'
+__credits__ = 'Universal Dependencies community'
+
+from .validator import Validator
