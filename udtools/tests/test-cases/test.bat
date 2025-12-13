659f982ef480f92bc754984cab8ac8b33d3f9df3
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-12-13 11:31:47 +0100

    Test cases updated to those currently used in master.

--- udtools/tests/test-cases/test.bat
+++ udtools/tests/test-cases/test.bat
@@ -1,13 +1,13 @@
 @echo off
 REM Test the UD validator on a set of short CoNLL-U files.
 REM The log file is versioned, so we can check git diff and see if it is OK.
-REM Usage: tools\test-cases\test.bat 2> tools\test-cases\test.log
+REM Usage: tools\validator\tests\test-cases\test.bat 2> tools\validator\tests\test-cases\test.log
 SET "SCRIPT_DIR=%~dp0"
 for %%f in ("%SCRIPT_DIR%invalid-level1-2\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 2 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 2 --lang ud "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
@@ -17,17 +17,34 @@ for %%f in ("%SCRIPT_DIR%invalid-level3\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 3 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 3 --lang ud "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
 @echo --------------------------------------------------------------------------------
 @echo LEVEL 4-5 TESTS 1>&2
-for %%f in ("%SCRIPT_DIR%invalid-level4-5\*.conllu") do (
+for %%f in ("%SCRIPT_DIR%invalid-level4-5\cs_*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 5 --lang cs "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 5 --lang cs "%%f"
+    @echo. 1>&2
+)
+for %%f in ("%SCRIPT_DIR%invalid-level4-5\yrl_*.conllu") do (
+    @echo -------------------------------------------------------------------------------- 1>&2
+    @echo %%f
+    @echo Processing file: %%f 1>&2
+    python %SCRIPT_DIR%..\..\..\validate.py --level 5 --lang yrl "%%f"
+    @echo. 1>&2
+)
+@echo -------------------------------------------------------------------------------- 1>&2
+@echo --------------------------------------------------------------------------------
+@echo TESTS BY LUDOVICA AND ARIANNA 1>&2
+for %%f in ("%SCRIPT_DIR%invalid-functions\*.conllu") do (
+    @echo -------------------------------------------------------------------------------- 1>&2
+    @echo %%f
+    @echo Processing file: %%f 1>&2
+    python %SCRIPT_DIR%..\..\..\validate.py --level 5 --lang cs "%%f"
     @echo. 1>&2
 )
 @echo -------------------------------------------------------------------------------- 1>&2
@@ -37,6 +54,6 @@ for %%f in ("%SCRIPT_DIR%valid\*.conllu") do (
     @echo -------------------------------------------------------------------------------- 1>&2
     @echo %%f
     @echo Processing file: %%f 1>&2
-    python %SCRIPT_DIR%..\validate.py --level 3 --lang ud "%%f"
+    python %SCRIPT_DIR%..\..\..\validate.py --level 3 --lang ud "%%f"
     @echo. 1>&2
 )
